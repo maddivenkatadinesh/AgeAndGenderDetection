3d706a8aa4c9f14e8a9bff332563ac2780af4f97
Maddi Venkata Dinesh <46440817+maddivenkatadinesh@users.noreply.github.com>
2021-07-13 20:28:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,156 @@
-from flask import Flask, render_template
-import sys
+from flask import Flask, render_template, Response, request
 import cv2
-import math
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
 import argparse
-#import os
+import math
 
-app=Flask(__name__)
+
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+#make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
+
+#Load pretrained face detection model    
+net = cv2.dnn.readNetFromCaffe('models/deploy.prototxt.txt', 'models/deploy_live.caffemodel')
+
+#instatiate flask app  
+app = Flask(__name__, template_folder='./templates')
+
+
+camera = cv2.VideoCapture(0)
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+
+def detect_face(frame):
+    global net
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))   
+    net.setInput(blob)
+    detections = net.forward()
+    confidence = detections[0, 0, 0, 2]
+
+    if confidence < 0.5:            
+            return frame           
+
+    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+    try:
+        frame=frame[startY:endY, startX:endX]
+        (h, w) = frame.shape[:2]
+        r = 480 / float(h)
+        dim = ( int(w * r), 480)
+        frame=cv2.resize(frame,dim)
+    except Exception as e:
+        pass
+    return frame
+ 
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame
+    while True:
+        success, frame = camera.read() 
+        if success:
+            if(face):                
+                frame= detect_face(frame)
+            if(grey):
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if(neg):
+                frame=cv2.bitwise_not(frame)    
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
+                cv2.imwrite(p, frame)
+            
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+
 
 @app.route('/')
-def default():
+def index():
     return render_template('index.html')
+    
+  
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('grey') == 'Grey':
+            global grey
+            grey=not grey
+        elif  request.form.get('neg') == 'Negative':
+            global neg
+            neg=not neg
+        elif  request.form.get('face') == 'Face Only':
+            global face
+            face=not face 
+            if(face):
+                time.sleep(4)   
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index.html')
+    return render_template('index.html')
+
 @app.route('/predict',methods=['GET','POST'])
 def predict():
 
@@ -41,9 +182,9 @@ def predict():
 
     faceProto="models/opencv_face_detector.pbtxt"
     faceModel="models/opencv_face_detector_uint8.pb"
-    ageProto="models/age1.prototxt"
+    ageProto="models/age_deploy.prototxt"
     ageModel="models/age_net.caffemodel"
-    genderProto="models/gender1.prototxt"
+    genderProto="models/gender_deploy.prototxt"
     genderModel="models/gender_net.caffemodel"
 
     MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
@@ -57,7 +198,7 @@ def predict():
     video=cv2.VideoCapture(args.image if args.image else 0)
     padding=20
     while cv2.waitKey(1)<0 :
-        hasFrame,frame=video.read()
+        hasFrame,frame=camera.read()
         if not hasFrame:
             cv2.waitKey()
             break
@@ -96,9 +237,19 @@ def predict():
             #data.save('gfg_dummy_pic.png')
             #save_path = os.path.join(save_folder, resultImg)
             #cv2.imshow("Detecting age and gender", resultImg)
-            #return render_template('index.html', resultImg=resultImg)
+            #return render_template('new 1.html', resultImg=resultImg)
     return 'dinesh'
     
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
+    
+camera.release()
+cv2.destroyAllWindows()     
+
+
+
+
+
+
+
+
